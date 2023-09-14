370fc725c69e7cfcab7925438e1b78f3ab2c366e
martinqiang <35414428+martinqiang@users.noreply.github.com>
2023-09-14 22:12:49 +1200

    Updated configuration for easier moving

---
+++ install-nvim.sh
@@ -0,0 +1,14 @@
+# for ubuntu
+
+# unstable nvim 
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt update
+
+sudo apt install neovim
+
+
+# install nodejs for CoC
+curl -sL install-node.vercel.app/lts | sudo bash
+
+
+echo "Run :PlugInstall in nvim"
---
+++ setup.sh
@@ -0,0 +1,8 @@
+cp .bashrc ~/.
+cp .gitconfig ~/.
+cp .vimrc ~/.
+
+cp -r .vim ~/.
+cp -r .ssh ~/.
+cp -r .config ~/.
+cp -r .git ~/.
